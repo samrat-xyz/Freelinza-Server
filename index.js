1a11ff59158a1bd7234e5302a6662a1405b11e48
Asif-vai <mr3366533@gmail.com>
2025-11-09 16:05:59 +0600

    created latest job api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = "mongodb+srv://FreelinzaDB:Cxr5YtPXkkhLByMk@cluster0.xq0m0kp.mongodb.net/?appName=Cluster0";
 const client = new MongoClient(uri, {
   serverApi: {
@@ -9,34 +10,44 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-const app = express();
-const port = process.env.PORT || 3030
-app.use(cors())
-app.use(express.json())
 
-app.get('/',(req,res)=>{
-    res.send('Freelinza Server Is Running')
-})
+const app = express();
+const port = process.env.PORT || 3030;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Freelinza Server Is Running');
+});
+
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const database = client.db("FreelinzaDB")
-    const freelinzaCollection = database.collection("freelancers")
-    app.post('/freelancers',async(req,res)=>{
-        const newFreelancer = req.body
-        const result = await freelinzaCollection.insertOne(newFreelancer)
-        res.send(result)
+    const database = client.db("FreelinzaDB");
+    const jobsCollection = database.collection("Jobs");
+
+    app.post('/jobs', async (req, res) => {
+        const newJob = req.body; 
+        const result = await jobsCollection.insertOne(newJob);
+        res.send(result);
+    });
+    app.get('/latest-jobs',async(req,res)=>{
+      const cursor = jobsCollection.find().skip(6).limit(6);
+      const result = await cursor.toArray();
+      res.send(result)
     })
-    // Send a ping to confirm a successful connection
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
+    // Client close optional
     // await client.close();
   }
 }
+
 run().catch(console.dir);
-app.listen(port,()=>{
-    console.log(`server is running on port ${port}`)
-})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
